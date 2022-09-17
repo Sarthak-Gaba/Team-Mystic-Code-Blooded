9b772a2cd677dc745190fb3b8225b220c47cce23
Anshjain123 <anshjainalp@gmail.com>
2022-09-17 13:19:42 +0530

    Search component ui

--- src/App.js
+++ src/App.js
@@ -9,82 +9,14 @@ import {
   deleteDoc,
   doc,
 } from "firebase/firestore";
+import Search from "./Components/Search";
 
 function App() {
-  const [newName, setNewName] = useState("");
-  const [newAge, setNewAge] = useState(0);
 
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
-
-  const createUser = async () => {
-    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
-  };
-
-  const updateUser = async (id, age) => {
-    const userDoc = doc(db, "users", id);
-    const newFields = { age: age + 1 };
-    await updateDoc(userDoc, newFields);
-  };
-
-  const deleteUser = async (id) => {
-    const userDoc = doc(db, "users", id);
-    await deleteDoc(userDoc);
-  };
-
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-      console.log(users);
-    };
-
-    getUsers();
-  }, []);
 
   return (
     <div className="App">
-      <h1>Welcome to Team Mystic!</h1>
-      <input
-        placeholder="Name..."
-        onChange={(event) => {
-          setNewName(event.target.value);
-        }}
-      />
-      <input
-        type="number"
-        placeholder="Age..."
-        onChange={(event) => {
-          setNewAge(event.target.value);
-        }}
-      />
-
-      <button onClick={createUser}> Create User</button>
-      {users.map((user) => {
-        return (
-          <div>
-            {" "}
-            <h1>Name: {user.name}</h1>
-            <h1>Age: {user.age}</h1>
-            <button
-              onClick={() => {
-                updateUser(user.id, user.age);
-              }}
-            >
-              {" "}
-              Increase Age
-            </button>
-            <button
-              onClick={() => {
-                deleteUser(user.id);
-              }}
-            >
-              {" "}
-              Delete User
-            </button>
-          </div>
-        );
-      })}
+      <Search />
     </div>
   );
 }
---
+++ src/Components/Search.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Accordian from './Accordian';
+import "./Search.css"
+
+const Search = () => {
+    return (
+        <div className="Container" style={{ minHeight: "90vh", height: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
+            <div className="search__container" style={{ display: 'flex', flexDirection: 'row', flex: 0.1, margin: "15px", alignItems: 'center', justifyContent: 'space-evenly', position: 'sticky', top: '30px' }}>
+                <div className="search">
+                    <TextField id="standard-basic" label="Search" variant="standard" style={{ width: '85vw' }} />
+                </div>
+                <div className="search__btn" >
+                    <Button variant="contained">Search</Button>
+                </div>
+            </div>
+            <div className="image__container" style={{ flex: 0.9, margin: '4.5vh', maxHeight:'60vh', overflowY:"scroll", borderRadius:'10px', padding:'4vw' }}>
+                {/* Image component will be here */}
+                <div className="accordians__container">
+                    <Accordian />
+                    <Accordian />
+                    <Accordian />
+                    <Accordian />
+                    <Accordian />
+                    <Accordian />
+                    <Accordian />
+                    <Accordian />
+                    <Accordian />
+                    <Accordian />
+                    <Accordian />
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Search
